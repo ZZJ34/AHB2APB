518515c0d1d9b5eb0c2e440e7dd8bb0587591303
zzj34 <zhaozjseu@163.com>
2022-03-21 17:05:46 +0800

    testbench基本内容

--- sim/test_bench.sv
+++ sim/test_bench.sv
@@ -8,8 +8,32 @@
 
 module test_bench();
 
+parameter PERIOD_CYCLE = 20 ;  // 20ns 50Mhz
+parameter DELAY_CYCLE  = 200;  // 200ns
+
 // global signal
 logic clk;
 logic rst_n;
+
+// APB interface
+apb_if i_apb_if(.hclk(clk), .hreset_n(rst_n));
+
+// APB top(wrapped)
+apb_top_wrapped i_apb_top_wrapped(._if(i_apb_if.AHB2APB));
+
+// clk signal
+initial begin
+    clk = 0;
+    forever begin
+        # (PERIOD_CYCLE/2) clk = ~clk;
+    end
+end
+
+// reset signal
+initial begin
+    rst_n = 0;
+    # DELAY_CYCLE
+    rst_n = 1;
+end
     
 endmodule
