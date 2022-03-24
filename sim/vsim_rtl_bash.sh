5a6590025b38edf949cfd03674303d68ca856330
zzj34 <zhaozjseu@163.com>
2022-03-24 16:05:34 +0800

    更新测试文件

--- sim/vsim_rtl_bash.sh
+++ sim/vsim_rtl_bash.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-# Compile necessary files
-#
 if [ ! -e work ];then
   vlib work
 fi
 
-vmap work work
-vlog timescale.v    # compile perameter
-vlog -f comp.all    # compile rtl
+vmap work work 
+vlog -f comp.all test_bench.sv
+vsim -onfinish stop -novopt -t ps -do "do wave.do; run -all" test_bench
